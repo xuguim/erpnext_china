6b676471aeadffc7894d0a8cd056044d6ddc8cd2
ilqc <iliuchao2326@163.com>
2024-11-20 08:24:10 +0800

    feat: hidden lead sidebar;permission_contact;original_lead's fields permission

--- erpnext_china/erpnext_china/custom_permission/contact/permission_contact.py
+++ erpnext_china/erpnext_china/custom_permission/contact/permission_contact.py
@@ -2,16 +2,35 @@ import frappe
 
 from erpnext_china.hrms_china.custom_form_script.employee.employee import get_employee_tree
 
+
+def get_contacts(users, ptype):
+	# 根据联系人下面关联的单据，如果用户有这个单据的权限，则对这个联系人也有权限
+	# 当前仅判断关联单据为客户的
+	contacts = []
+	links = frappe.get_all("Dynamic Link", filters={"link_doctype": "Customer", "parentfield": "links", "parenttype": "Contact"}, fields=["*"])
+	for link in links:
+		for user in users:
+			if frappe.has_permission("Customer", ptype=ptype, doc=link.link_name, user=user):
+				contacts.append(link.parent)
+	return contacts
+
 def has_query_permission(user):
 	if frappe.db.get_value('Has Role',{'parent':user,'role':'System Manager'}):
 		# 如果角色包含管理员，则看到全量
 		conditions = ''
 	else:
-		# 其他情况则只能看到自己,上级可以看到下级
+		# 上级可以看到下级创建的联系人
 		users = get_employee_tree(parent=user)
 		users.append(user)
+		
 		users_str = str(tuple(users)).replace(',)',')')
 		conditions = f"(tabContact.owner in {users_str})" 
+
+		# 上级也可以看到下级拥有联系人关联客户单据权限的联系人
+		contacts = get_contacts(users, 'read')
+		if len(contacts) > 0:
+			contacts_str = str(tuple(contacts)).replace(',)',')')
+			conditions += f"or (tabContact.name in {contacts_str})"
 	return conditions
 
 def has_permission(doc, user, permission_type=None):
@@ -19,10 +38,11 @@ def has_permission(doc, user, permission_type=None):
 		# 如果角色包含管理员，则看到全量
 		return True
 	else:
-		# 其他情况则只能看到自己,上级可以看到下级
 		users = get_employee_tree(parent=user)
 		users.append(user)
-		if (doc.owner in users):
+
+		contacts = get_contacts(users, 'write')
+		if (doc.owner in users) or (doc.name in contacts):
 			return True
 		else:
 			return False
--- erpnext_china/erpnext_china/doctype/original_leads/original_leads.js
+++ erpnext_china/erpnext_china/doctype/original_leads/original_leads.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2024, Digitwise Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Original Leads", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Original Leads", {
+	refresh(frm) {
+        // 如果是销售，则隐藏
+        const readOnly = frappe.user.has_role('销售')
+        const fields = ['ad_attributes_section', 'section_break_ymsv', 'original_json_data_section']
+        if (readOnly) {
+            fields.forEach(field => {
+                frm.fields_dict[field].df.hidden = true;
+                frm.refresh_field(field);
+            });
+        }
+	},
+});
