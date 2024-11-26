4f96575cc38aa6d543d839960fededc019901282
digitwise <407469895@qq.com>
2024-11-27 01:58:56 +0800

    优化从批号名称中生成生产日期

--- erpnext_china/erpnext_china/custom_form_script/batch/batch.py
+++ erpnext_china/erpnext_china/custom_form_script/batch/batch.py
@@ -17,11 +17,33 @@ class CustomBatch(Batch):
                 title='警告',
                 raise_exception=FileNotFoundError
             )
+
+
         # 根据生产日期验证批号是否正确
-        if frappe.utils.getdate(self.manufacturing_date).strftime('%y%m%d') not in self.name:
-            if not frappe.db.get_value('Has Role',{'parent':self.modified_by,'role':'仓库管理'}):
-                frappe.msgprint(
-                    msg='批号名称与生产日期不具备一致性，请核实后重新输入。（如果批号本身与生产日期不具备一致性，请联系管理人员录入）',
-                    title='警告',
-                    raise_exception=FileNotFoundError
-                    )
+        def check_manufactruing_date_in_batch_no(self):
+            '''验证生产日期不超出批号30天'''
+            manufacturing_date = frappe.utils.getdate(self.manufacturing_date)
+            for days in list(range(1,31)):
+                result = False
+                if frappe.utils.add_to_date(manufacturing_date, days=days).strftime('%y%m%d') in self.name:
+                    result = True
+                    break
+            return result
+
+        if self.custom_batchno_with_manufacturing_date == 1:
+            # 选择从批号中创建生产日期
+            year_pos = 999
+            get_years = lambda: [str(datetime.datetime.now().year - i)[2:] for i in range(4)]
+            for year in get_years():
+                if self.name.find(year) != -1:
+                    if self.name.find(year) < year_pos:
+                        year_pos = self.name.find(year)
+            manufactruing_date = frappe.utils.getdate(datetime.datetime.strptime(self.name[year_pos:year_pos+6],'%y%m%d'))
+        else:
+            if not check_manufactruing_date_in_batch_no(self):
+                if not frappe.db.get_value('Has Role',{'parent':self.modified_by,'role':'仓库管理11'}):
+                    frappe.msgprint(
+                        msg='批号名称与生产日期不具备一致性，请核实后重新输入。（如果批号本身与生产日期不具备一致性，请联系管理人员录入）',
+                        title='警告',
+                        raise_exception=FileNotFoundError
+                        )
