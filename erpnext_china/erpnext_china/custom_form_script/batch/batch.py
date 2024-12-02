1f22b1f9d4ce802066690e7d45e520066254abc2
digitwise <407469895@qq.com>
2024-12-02 15:29:20 +0800

    Update batch.py

--- erpnext_china/erpnext_china/custom_form_script/batch/batch.py
+++ erpnext_china/erpnext_china/custom_form_script/batch/batch.py
@@ -29,7 +29,7 @@ class CustomBatch(Batch):
             self.manufacturing_date = datetime.datetime.strptime(self.name[year_pos:year_pos+6],'%y%m%d')
         else:
             if not check_manufactruing_date_in_batch_no(self):
-                if not frappe.db.get_value('Has Role',{'parent':self.modified_by,'role':'仓库管理11'}):
+                if not frappe.db.get_value('Has Role',{'parent':self.modified_by,'role':'仓库管理'}):
                     frappe.msgprint(
                         msg='批号名称与生产日期不具备一致性，请核实后重新输入。（如果批号本身与生产日期不具备一致性，请联系管理人员录入）',
                         title='警告',
