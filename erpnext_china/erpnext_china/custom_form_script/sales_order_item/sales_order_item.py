0a0deefdfea4a35e670145ee2e5ce61c03b1be86
allen.xu <xu.gui.m@gmail.com>
2025-01-15 21:23:31 +0800

    feat: rate calc whith  additional discount for sales order item

--- erpnext_china/erpnext_china/custom_form_script/sales_order_item/sales_order_item.py
+++ erpnext_china/erpnext_china/custom_form_script/sales_order_item/sales_order_item.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils import flt
 from erpnext.selling.doctype.sales_order_item.sales_order_item import SalesOrderItem
 from erpnext.stock.dashboard.item_dashboard import get_data
 
@@ -9,3 +10,19 @@ class CustomSalesOrderItem(SalesOrderItem):
 		if len(items) > 0:
 			return items[0].actual_qty
 		return 0
+	
+	@property
+	def rate_after_discount(self):
+		if self.is_new():
+			frappe.log('new')
+			return 0
+		so_info = frappe.get_all("Sales Order", filters={'name':self.parent}, fields=["grand_total","discount_amount"])[0]
+		frappe.log(so_info)
+		if flt(so_info.discount_amount) == 0 or flt(so_info.grand_total) == 0:
+			return self.rate
+		else:
+			return self.rate * ( 1 - self.discount_amount / so_info.grand_total)
+		
+	@property
+	def rate_after_discount_of_stock_uom(self):
+		return self.rate_after_discount / self.conversion_factor
